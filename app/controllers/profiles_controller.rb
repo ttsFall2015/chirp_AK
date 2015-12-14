262877e69496285c7a9222c44f8b6967c79c056e
“AK <hello@annakateselby.com>
2015-12-14 17:18:11 -0500

    test commit

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,8 +2,10 @@ class ProfilesController < ApplicationController
 
   def show
     @users = User.find(params[:id])
+    # Creates a show method called users which finds a user
   end
 
   def index
     @users = User.all
+    # Creates an index method called users which applies to all users
 end
